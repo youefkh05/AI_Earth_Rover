57d38c30723cab3b7db46a5fa4222aa8088eee89
youefkh05 <youefkh05@gmail.com>
2025-03-21 11:15:19 +0200

    Added initial files
    from the repo:
    https://github.com/frodobots-org/earth-rovers-sdk

---
+++ browser_service.py
@@ -0,0 +1,138 @@
+import os
+import time
+from pyppeteer import launch
+
+
+class BrowserService:
+    def __init__(self):
+        self.browser = None
+        self.page = None
+        self.default_viewport = {"width": 3840, "height": 2160}
+
+    async def initialize_browser(self):
+        if not self.browser:
+            try:
+                executable_path = os.getenv(
+                    "CHROME_EXECUTABLE_PATH",
+                    "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+                )
+                self.browser = await launch(
+                    executablePath=executable_path,
+                    headless=True,
+                    args=[
+                        "--ignore-certificate-errors",
+                        "--no-sandbox",
+                        f"--window-size={self.default_viewport['width']},{self.default_viewport['height']}",
+                    ],
+                )
+                self.page = await self.browser.newPage()
+                await self.page.setViewport(self.default_viewport)
+                await self.page.setExtraHTTPHeaders(
+                    {"Accept-Language": "en-US,en;q=0.9"}
+                )
+                await self.page.goto(
+                    "http://127.0.0.1:8000/sdk", {"waitUntil": "networkidle2"}
+                )
+                await self.page.click("#join")
+                await self.page.waitForSelector("video")
+                await self.page.waitForSelector("#map")
+                await self.page.setViewport(self.default_viewport)
+
+                await self.page.waitFor(2000)
+            except Exception as e:
+                print(f"Error initializing browser: {e}")
+                self.browser = None
+                self.page = None
+                await self.close_browser()
+                raise
+
+    async def take_screenshot(self, video_output_folder: str, elements: list):
+        await self.initialize_browser()
+
+        dimensions = await self.page.evaluate(
+            """() => {
+            return {
+                width: Math.max(document.documentElement.scrollWidth, window.innerWidth),
+                height: Math.max(document.documentElement.scrollHeight, window.innerHeight),
+            }
+        }"""
+        )
+
+        if (
+            dimensions["width"] > self.default_viewport["width"]
+            or dimensions["height"] > self.default_viewport["height"]
+        ):
+            await self.page.setViewport(dimensions)
+
+        element_map = {"front": "#player-1000", "rear": "#player-1001", "map": "#map"}
+
+        screenshots = {}
+        for name in elements:
+            if name in element_map:
+                element_id = element_map[name]
+                output_path = f"{video_output_folder}/{name}.png"
+                element = await self.page.querySelector(element_id)
+                if element:
+                    start_time = time.time()  # Start time
+                    await element.screenshot({"path": output_path})
+                    end_time = time.time()  # End time
+                    elapsed_time = (
+                        end_time - start_time
+                    ) * 1000  # Convert to milliseconds
+                    print(f"Screenshot for {name} took {elapsed_time:.2f} ms")
+                    screenshots[name] = output_path
+                else:
+                    print(f"Element {element_id} not found")
+            else:
+                print(f"Invalid element name: {name}")
+
+        return screenshots
+
+    async def data(self) -> dict:
+        await self.initialize_browser()
+
+        bot_data = await self.page.evaluate(
+            """() => {
+        return window.rtm_data;
+        }"""
+        )
+
+        return bot_data
+
+    async def front(self) -> str:
+        await self.initialize_browser()
+
+        front_frame = await self.page.evaluate(
+            """() => {
+        return lastBase64Frames[1000] || null;
+        }"""
+        )
+
+        return front_frame
+
+    async def rear(self) -> str:
+        await self.initialize_browser()
+
+        rear_frame = await self.page.evaluate(
+            """() => {
+        return lastBase64Frames[1001] || null;
+        }"""
+        )
+
+        return rear_frame
+
+    async def send_message(self, message: dict):
+        await self.initialize_browser()
+
+        await self.page.evaluate(
+            """(message) => {
+                window.sendMessage(message);
+            }""",
+            message,
+        )
+
+    async def close_browser(self):
+        if self.browser:
+            await self.browser.close()
+            self.browser = None
+            self.page = None
